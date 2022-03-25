40084e32dbe0cfd3522405cc45ae0efae4b1dde8
ichu4321 <ianchu@umich.edu>
2022-03-25 14:18:38 -0400

    changed test.py to main.py and cleaned it up

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from scraper import Scraper
 
 
 if __name__ == "__main__":
+	# list of search terms
+	MAX_IMAGES_PER_TERM = 200;
+	search_terms = [
+	"grass",
+	"field",
+	"lawn",
+	"yard"
+	]
+
 	# set up the scraper with the fast duplication check
 	webscraper = Scraper(hashes.fastHash, save_duplicates = True);
 
 	# scrape images
-	webscraper.getImages("rice", 120);
-	# webscraper.getImages("yard", 100);
-
+	for term in search_tersm:
+		webscraper.getImages(term, MAX_IMAGES_PER_TERM);
